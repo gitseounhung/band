4eba5b46300f955c92746fe57ccdbf2aca87a1eb
shindalsoo <shindalsoo@naver.com>
2024-09-09 09:46:52 +0900

    모달컴포넌트 수정

--- packages/components/modal/createChannel.js
+++ packages/components/modal/createChannel.js
@@ -1,6 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useAppSelector } from '@zio/shared/redux/hooks'
+import toast from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
 
 const ModalCreateChannel = ({modal, onClose}) => {
+  const [input,setInput] = useState()
+  const session = useAppSelector(state=>state.session)
+  const navigate = useNavigate()
+  
+  const handleOnChange = (e) => {
+    setInput(e.target.value)
+  }
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+    e.stopPropagation()
+    const URL = `${process.env.REACT_APP_BACKEND_URL}/api/create-channel`
+    try {
+      console.log('input',input)
+      const response = await axios.post(URL,{
+        userId: session?._id,
+        channelName: input,
+      })
+      toast.success(response.data.message)
+      if (response.data.success){
+        console.log('response',response.data.data)
+        setInput('')
+        navigate('/chat/'+response.data.data._id)
+      }
+    } catch(error){
+      console.log('error',error)
+      toast.error(error?.response?.data?.message)
+    }
+    onClose()
+  }
+
   return (
     <div 
       className={`modal fade effect-scale ${modal==='CreateChannel' ? "show" : "d-none"}`}
@@ -23,11 +57,17 @@ const ModalCreateChannel = ({modal, onClose}) => {
               <span aria-hidden="true"></span>
             </button>
             <h6 className="tx-uppercase tx-spacing-1 tx-semibold mg-b-20">채널 만들기</h6>
-            <input type="text" className="form-control" placeholder="채널명을 입력하세요." value="" />
+            <input 
+              type="text" 
+              className="form-control" 
+              placeholder="채널명을 입력하세요." 
+              onChange={handleOnChange}
+              value={input} 
+            />
           </div>
           <div className="modal-footer pd-x-20 pd-b-20 pd-t-0 bd-t-0">
             <button type="button" className="btn btn-secondary tx-13" data-dismiss="modal" onClick={onClose}>취소</button>
-            <button type="button" className="btn btn-primary tx-13">생성</button>
+            <button type="button" className="btn btn-primary tx-13" onClick={handleSubmit}>생성</button>
           </div>
         </div>
       </div>
