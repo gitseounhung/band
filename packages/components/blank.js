1590c3885fac794de1fefa5470d5206297370e88
shindalsoo <shindalsoo@naver.com>
2024-10-31 15:30:04 +0900

    dfdfd

--- packages/components/blank.js
+++
@@ -1,9 +0,0 @@
-import React,{Fragment} from 'react'
-
-const Blank = () => {
-  return (
-    <Fragment></Fragment>
-  )
-}
-
-export default Blank
--- packages/shared/data/index.js
+++
@@ -1,9 +0,0 @@
-import menuData from './menu'
-import filesData from './files'
-import foldersData from './folders'
-
-export {
-  menuData,
-  filesData,
-  foldersData
-}
